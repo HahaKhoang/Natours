cebc1ad017f94d8c36bc760b891452aea06f3d06
Kathy Hoang <kathyhahoang@gmail.com>
2023-06-02 15:55:04 -0500

    Populates tour model

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
@@ -68,6 +69,7 @@ app.use((request, response, next) => {
 // 2. ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 app.all('*', (request, response, next) => {
   next(
