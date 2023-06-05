9b7d0276531908ac0f7a9204adea5fc74512e4ce
Kathy Hoang <kathyhahoang@gmail.com>
2023-06-05 14:07:45 -0500

    Creates factory function for delete

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,5 +1,6 @@
 const Review = require('./../models/reviewModel');
 const catchAsync = require('./../utils/catchAsync');
+const factory = require('./handlerFactory');
 
 exports.getAllReviews = catchAsync(
   async (request, response, next) => {
@@ -31,3 +32,5 @@ exports.createReview = catchAsync(async (request, response, next) => {
     },
   });
 });
+
+exports.deleteReview = factory.deleteOne(Review);
