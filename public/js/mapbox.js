56ab2ea2849e09f7fcc4736b098db13c876d70ce
Kathy Hoang <kathyhahoang@gmail.com>
2023-06-08 10:57:49 -0500

    Adds map to page

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,6 +1,17 @@
 /* eslint-disable */
+
 const locations = JSON.parse(
   document.getElementById('map').dataset.locations
 );
 
 console.log(locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiaGFoYWhvYW5nIiwiYSI6ImNsaW5iZHluejBqd2YzcnRod2RydGJ1bnAifQ.2ZScbLOTbhLxDGZ2i_g1sw';
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/hahahoang/clinbivpo01bn01p65gb1fw14',
+  center: [-118.113491, 34.111745],
+  zoom: 4,
+  interactive: false,
+});
