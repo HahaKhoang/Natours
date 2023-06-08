d5c6f9e144d2adfcb3222ef5f52c1b6bbd5d9d9f
Kathy Hoang <kathyhahoang@gmail.com>
2023-06-08 11:11:44 -0500

    Completes map

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -11,7 +11,42 @@ mapboxgl.accessToken =
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/hahahoang/clinbivpo01bn01p65gb1fw14',
-  center: [-118.113491, 34.111745],
-  zoom: 4,
-  interactive: false,
+  scrollZoom: false,
+  //   center: [-118.113491, 34.111745],
+  //   zoom: 4,
+  //   interactive: false,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((location) => {
+  // Create marker
+  const element = document.createElement('div');
+  element.className = 'marker';
+
+  // Add marker
+  new mapboxgl.Marker({
+    element: element,
+    anchor: 'bottom',
+  })
+    .setLngLat(location.coordinates)
+    .addTo(map);
+
+  // Add popup
+  new mapboxgl.Popup({ offset: 30 })
+    .setLngLat(location.coordinates)
+    .setHTML(`<p>Day ${location.day}: ${location.description}</p>`)
+    .addTo(map);
+
+  // Extend map bounds to include current location
+  bounds.extend(location.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
 });
