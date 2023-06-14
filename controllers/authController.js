a985b098d666993cb53424f62243c7a13f72cbdc
Kathy Hoang <kathyhahoang@gmail.com>
2023-06-14 12:06:53 -0500

    Adds new Email class to signup handler

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
-const sendEmail = require('./../utils/email');
+const Email = require('./../utils/email');
 
 const signToken = (id) =>
   jwt.sign({ id: id }, process.env.JWT_SECRET, {
@@ -46,7 +46,9 @@ exports.signup = catchAsync(async (request, response, next) => {
     passwordChangedAt: request.body.passwordChangedAt,
     role: request.body.role,
   });
-
+  const url = `${request.protocol}://${request.get('host')}/me`;
+  console.log(url);
+  await new Email(newUser, url).sendWelcome();
   createSendToken(newUser, 201, response);
 });
 
@@ -206,11 +208,11 @@ exports.forgotPassword = catchAsync(
 
     const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
     try {
-      await sendEmail({
-        email: user.email,
-        subject: 'Your password reset token (valid for 10 min)',
-        message,
-      });
+      //   await sendEmail({
+      //     email: user.email,
+      //     subject: 'Your password reset token (valid for 10 min)',
+      //     message,
+      //   });
 
       response.status(200).json({
         status: 'success',
--- utils/email.js
+++ utils/email.js
@@ -30,7 +30,7 @@ module.exports = class Email {
   async send(template, subject) {
     // 1. Render HTML based on a pug template
     const html = pug.renderFile(
-      `${__dirname}/../views/emails/${template}.pug`,
+      `${__dirname}/../views/email/${template}.pug`,
       {
         firstName: this.firstName,
         url: this.url,
@@ -43,12 +43,11 @@ module.exports = class Email {
       to: this.to,
       subject: subject,
       html,
-      text: htmlToText.fromString(html),
+      text: htmlToText.htmlToText(html),
       // html:
     };
 
     // 3. Create a transport and send email
-
     await this.newTransport().sendMail(mailOptions);
   }
 
