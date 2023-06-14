67adafc5f152ea44f7be1174676844b46ed0dbd4
Kathy Hoang <kathyhahoang@gmail.com>
2023-06-14 12:26:23 -0500

    Creates sendPasswordReset method

--- controllers/authController.js
+++ controllers/authController.js
@@ -202,17 +202,12 @@ exports.forgotPassword = catchAsync(
     await user.save({ validateBeforeSave: false });
 
     // 3. Send it to user's email
-    const resetURL = `${request.protocol}://${request.get(
-      'host'
-    )}/api/v1/users/resetPassword/${resetToken}`;
-
-    const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
     try {
-      //   await sendEmail({
-      //     email: user.email,
-      //     subject: 'Your password reset token (valid for 10 min)',
-      //     message,
-      //   });
+      const resetURL = `${request.protocol}://${request.get(
+        'host'
+      )}/api/v1/users/resetPassword/${resetToken}`;
+
+      await new Email(user, resetURL).sendPasswordReset();
 
       response.status(200).json({
         status: 'success',
