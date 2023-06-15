7ae4cc157261e990c7b3fb6b5d40f78d06423ef3
Kathy Hoang <kathyhahoang@gmail.com>
2023-06-15 14:30:18 -0500

    Creates endpoints for bookings

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -62,3 +62,9 @@ exports.createBookingCheckout = catchAsync(
     response.redirect(request.originalUrl.split('?')[0]);
   }
 );
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
